ad4ae0834bf8dca5523d2adebc6f2dab52f481bb
Zavorotna <zavorotnaa96@gmail.com>
2023-04-15 16:24:31 +0300

    misocial

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,11 @@
 const burger = document.querySelector('.burger');
+const darkburger = document.querySelector('.darkburger');
     const mobileMenu = document.querySelector('.mobile-menu');
     burger.addEventListener('click', function() {
       this.classList.toggle('active');
+      mobileMenu.classList.toggle('activemobile');
+    })
+    darkburger.addEventListener('click', function() {
+      this.classList.toggle('activedark');
       mobileMenu.classList.toggle('activemobile');
     })
